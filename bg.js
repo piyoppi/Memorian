a0014241ae5108174aa3d66b4477f6aa48fbf840
piyoppi <3podvator@gmail.com>
2017-07-13 01:26:05 +0900

    要素の情報を取得できるようにした

---
+++ bg.js
@@ -0,0 +1,26 @@
+
+function contextMenu_Click(info, tab){
+    chrome.tabs.sendMessage(tab.id, {hoge: "text_memo"}, function(response) {
+        alert(response.selection_text);
+    });
+}
+
+function mnu_ElementMemo_click(info, tab){
+    chrome.tabs.sendMessage(tab.id, {id: "element_memo"}, function(response) {
+        alert(response.selection_elem);
+    });
+}
+
+
+
+chrome.contextMenus.create({
+    title: "この部分を切り抜く",
+    contexts: ["selection"],
+    onclick: contextMenu_Click
+});
+
+chrome.contextMenus.create({
+    title: "この部分を切り抜く",
+    onclick: mnu_ElementMemo_click
+});
+
--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 
+var curpos = {x: 0, y: 0};
+
+
+function find_element_fromtext(text){
+
+}
+
+function find_element_fromcurpos(){
+    var setpos_x = window.pageXOffset + curpos.x;
+    var setpos_y = window.pageYOffset + curpos.y;
+    var obj = document.elementFromPoint(curpos.x, curpos.y);
+    return obj;
+}
+
+function handler_mousemove(e){
+    curpos.x = e.clientX;
+    curpos.y = e.clientY;
+}
+
+document.addEventListener("contextmenu", handler_mousemove, false);
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-      alert( request.hoge );
-      sendResponse({title: document.title});
+      if( request.id = "element_memo" ){
+          console.log( find_element_fromcurpos().innerHTML );
+          sendResponse({selection_elem: find_element_fromcurpos().innerHTML});
+      }
+      else{
+          sendResponse({selection_text: window.getSelection()});
+      }
   }
 );
+
+
