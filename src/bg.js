c849f65fdc417d7fcfcf942c9e5a0b6bbf7e6169
piyoppi <3podvator@gmail.com>
2017-07-17 11:57:56 +0900

    ディレクトリ構造を整理

--- src/bg.js
+++ src/bg.js
@@ -30,7 +30,6 @@ chrome.contextMenus.create({
     onclick: mnu_ElementMemo_click
 });
 
-
 window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
 window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
